c1edcea053fd87e5501fc82456a5d652175d5eff
MinesJA <minesja@gmail.com>
2020-04-13 17:10:03 -0400

    Merging in index.md changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,50 +1,104 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
 
-export const IndexPageTemplate = ({image, title, heading, blurb}) => (<div>Hello</div>)
+export const IndexPageTemplate = ({
+  image,
+  title,
+  heading,
+  blurb
+}) => (
+  <div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
 
+          <div className="columns">
+
+            {/* Image */}
+            <div className="column is-4">
+              <PreviewCompatibleImage imageInfo={image} />
+            </div>
+
+
+            <div className="column">
+              <div className="content">
+                
+                <div className="columns">
+                  <div className="column is-12">
+                    <h3 className="has-text-weight-semibold is-size-2">
+                      {heading}
+                    </h3>
+                    <p>{blurb}</p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="section">
+          <div className="tile is-ancestor">
+            
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+)
+
+IndexPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  blurb: PropTypes.string,
+}
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <IndexPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        blurb={frontmatter.blurb}
+      />
+    </Layout>
+  )
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
 
 export default IndexPage
 
-// export const pageQuery = graphql`
-  // query IndexPageTemplate {
-  //   markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-  //     frontmatter {
-  //       title
-  //       image {
-  //         childImageSharp {
-  //           fluid(maxWidth: 2048, quality: 100) {
-  //             ...GatsbyImageSharpFluid
-  //           }
-  //         }
-  //       }
-  //       heading
-  //       subheading
-  //       mainpitch {
-  //         title
-  //         description
-  //       }
-//         description
-//         intro {
-//           blurbs {
-//             image {
-//               childImageSharp {
-//                 fluid(maxWidth: 240, quality: 64) {
-//                   ...GatsbyImageSharpFluid
-//                 }
-//               }
-//             }
-//             text
-//           }
-//           heading
-//           description
-//         }
-//       }
-//     }
-//   }
-// `
+
+export const pageQuery = graphql`query IndexPageTemplate {
+  markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
+    frontmatter {
+      title
+      image {
+        childImageSharp {
+          fluid(maxWidth: 240, quality: 64) {
+           ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      heading
+      blurb
+    }
+  }
+}`
